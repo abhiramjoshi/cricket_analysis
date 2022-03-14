d967d7a975f7435723824439b8ef9ea3a39e4700
Abhi_Joshi <joshi.abhiram98@gmail.com>
2022-03-14 10:54:03 +0000

    Match_data class

--- codebase/match_data.py
+++ codebase/match_data.py
@@ -6,15 +6,18 @@ from concurrent.futures import as_completed
 from requests_futures.sessions import FuturesSession
 import json
 from codebase.settings import DATA_LOCATION
+from utils import logger
+import pickle
 
 DETAILED_COMMS_BASE_URL = 'https://hs-consumer-api.espncricinfo.com/v1/pages/match/comments?lang=en&seriesId={seriesid}&matchId={matchid}&inningNumber={inning}&commentType=ALL&sortDirection=DESC'
 
 class MatchData(Match):
 
-    def __init__(self, match_id, try_local = True):
-        super().__init__(match_id)
+    def __init__(self, match_id, try_local=True):
+        super().__init__(match_id, try_local)
         self.detailed_comms_url = DETAILED_COMMS_BASE_URL.replace('{seriesid}', str(self.series_id)).replace('{matchid}', str(self.match_id))
         self.full_comms = self.get_detailed_comms_faster(try_local = try_local)
+        self.all_players = self.team_1_players + self.team_2_players
         self.first_inning = self.get_innings_comms(innings = 1)
         self.second_innings = self.get_innings_comms(innings = 2)
         self.third_innings = self.get_innings_comms(innings = 3)
@@ -24,40 +27,43 @@ class MatchData(Match):
         """
         Detailed commentary for the whole match
         """
+        logger.info(f'{self.match_id}: Loading match comms')
         try:
             if try_local:
                 if os.path.exists(os.path.join(DATA_LOCATION, f'{self.match_id}_full_comms.json')):
+                    logger.info(f'{self.match_id}: Loading commentary from local storage')
                     with open(os.path.join(DATA_LOCATION, f'{self.match_id}_full_comms.json'), 'r') as jf:
                         return json.load(jf)
+
+            logger.info(f'{self.match_id}: Loading commentary from Cricinfo')
             full_comms = []
             for innings in self.innings_list: ##Sessionize innings search and parallize overs.
+                innings_comm_buffer = []
                 INNINGS_URL = self.detailed_comms_url.replace('{inning}', str(innings['innings_number']))
                 with FuturesSession() as inning_session:
-                    print(f'Fetching inning {innings["innings_number"]} comms')
+                    logger.info(f'Fetching inning {innings["innings_number"]} commentary')
                     init_future = inning_session.get(INNINGS_URL)
                     init_response = init_future.result()
                     overs = init_response.json()['nextInningOver'] 
                     if overs == 'null' or overs is None:
                         continue
                     
-                    full_comms += init_response.json()['comments']
+                    innings_comm_buffer += init_response.json()['comments']
                     concurrent_requests = int(overs)//2
                     
                     innings_comms = [inning_session.get(INNINGS_URL+f'&fromInningOver={(x+1)*2}') for x in reversed(range(concurrent_requests))]
                     for chunk in innings_comms:
-                        full_comms += chunk.result().json()['comments']
-                    print('Innings completed')
+                        innings_comm_buffer += chunk.result().json()['comments']
+                    logger.info('Innings completed')
+                    full_comms.append(list(reversed(innings_comm_buffer)))
             if save:
-                self.save_json_data(full_comms, 'full_comms')
+                self.save_data(data=full_comms, suffix='full_comms')
             return full_comms
-        except:
-            print("Init Response Innings: \n",init_response)
-            print("Innings: \n", innings_comms)
-
-
-    def save_json_data(self, data, suffix):
-        with open(os.path.join(DATA_LOCATION, f'{self.match_id}_{suffix}.json'), 'w') as f:
-            f.write(json.dumps(data))
+        except TypeError:
+            logger.warning(f'Match {self.match_id}, {self.match_title} does not have commentary data')
+        except IndexError:
+            logger.warning("Init Response Innings: \n",init_response)
+            logger.warning("Innings: \n", innings_comms)
 
     def get_detailed_comms(self):
         """
@@ -82,8 +88,18 @@ class MatchData(Match):
             print(response)
 
     def get_innings_comms(self, innings):
-        pass
+        try:
+            return self.full_comms[innings-1]
+        except (IndexError, TypeError):
+            return None
 
-
+    def get_full_comms(self):
+        """
+        Flatten and return full comms
+        """
+        if self.full_comms is None:
+            return None
+        return [ball for innings in self.full_comms for ball in innings]
+       
 if __name__ == '__main__':
     pass
