7c9c1b8734e3ea6146a314041de875fc6c5a2888
Abhi_Joshi <joshi.abhiram98@gmail.com>
2022-03-14 10:55:17 +0000

    settings and utils

--- codebase/settings.py
+++ codebase/settings.py
@@ -2,3 +2,9 @@ import os
 
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_LOCATION = os.path.join(BASE_PATH, "data")
+MATCH_JSONS = os.path.join(BASE_PATH, "data", 'match_jsons')
+HTMLS = os.path.join(BASE_PATH, "data", 'htmls')
+
+for _path in [DATA_LOCATION, MATCH_JSONS, HTMLS]:
+    if not os.path.exists(_path):
+        os.mkdir(_path)
---
+++ utils.py
@@ -0,0 +1,32 @@
+import logging
+import os
+from datetime import datetime
+
+LOGDIR = './logs'
+
+if not os.path.exists(LOGDIR):
+    os.mkdir(LOGDIR)
+
+LOGFILE = os.path.join(LOGDIR, f'cricket_data_log_{datetime.now().strftime("%Y_%m_%d_%H%M")}.log')
+WARNINGS_LOG = os.path.join(LOGDIR, f'cricket_data_warnings.log')
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+f_logger = logging.FileHandler(LOGFILE)
+s_logger = logging.StreamHandler()
+
+f_logger.setLevel(logging.DEBUG)
+s_logger.setLevel(logging.INFO)
+
+f_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+s_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+f_logger.setFormatter(f_formatter)
+s_logger.setFormatter(s_formatter)
+
+logger.addHandler(f_logger)
+logger.addHandler(s_logger)
+
+class NoMatchCommentaryError(Exception):
+    pass
