ef825960fc443444d3117be32c5ca8f0608d9bf6
Abhi_Joshi <joshi.abhiram98@gmail.com>
2022-03-21 23:49:09 +0000

    Data folder creation

--- codebase/settings.py
+++ codebase/settings.py
@@ -2,9 +2,20 @@ import os
 
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_LOCATION = os.path.join(BASE_PATH, "data")
+FULL_COMMS = os.path.join(BASE_PATH, "data", 'full_comms')
 MATCH_JSONS = os.path.join(BASE_PATH, "data", 'match_jsons')
 HTMLS = os.path.join(BASE_PATH, "data", 'htmls')
+ANALYSIS = os.path.join(BASE_PATH, "data", 'analysis_data')
 
-for _path in [DATA_LOCATION, MATCH_JSONS, HTMLS]:
+
+paths = [
+    DATA_LOCATION,
+    FULL_COMMS,
+    MATCH_JSONS,
+    HTMLS,
+    ANALYSIS
+]
+
+for _path in paths:
     if not os.path.exists(_path):
         os.mkdir(_path)
