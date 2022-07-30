aebf62a38c09e02a5e15f60c812ced74c40d025a
Abhi_Joshi <joshi.abhiram98@gmail.com>
2022-07-30 09:34:54 +0100

    label and parse functions for commentary

--- labeller.py
+++ labeller.py
@@ -2,7 +2,7 @@ from codebase.settings import LABEL_DATA
 import os
 from pprint import pprint
 
-FILE = '/Users/abhiramjoshi/Documents/Personal/cricket_analysis/data/drive_dismissals_kohli.txt'
+FILE = 'D:\Documents\Cricket\data_analysis\cricket_data_analysis\data\drive_dismisals.txt'
 PROMPT = 'Example of cover drive dismissal?'
 OPTIONS = [
     "Yes",
@@ -10,23 +10,37 @@ OPTIONS = [
 ]
 OPTIONS = {i:key for i,key in enumerate(OPTIONS)}
 
-SAVEFILE = os.path.join(LABEL_DATA, f'labelled_{FILE}')
+selections = ['0', '1', '0', '1', '0', '0', '0', '1', '1', '0', '0', '0', '1', '0', '0', '1', '1', '0', '0', '0', '0', '0', '0', '0', '1', '0', '1', '0', '0', '1', '0', '0', '0', '1', '1', '0', '1', '1', '0', '1', '1', '0', '0', '1', '0', '1', '1', '0', '1', '0', '1', '0', '1', '0', '0', '0', '0', '0', '1', '0', '0', '0', '1', '0', '1', '0', '1', '0', '0', '0', '0', '1', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0']
+
+SAVEFILE = os.path.join(LABEL_DATA, f'labelled_drive_dismisals.txt')
 
 labels = []
 print(f'Reading file from {FILE}\n---------------------\n')
 
-with open(FILE, 'r') as file:
-    for line in file.readlines():
-        print(line)
-        print(f'{PROMPT}\nSelect the number for the valid option:')
-        pprint(OPTIONS)
-        label = int(input('Select an option: '))
-        labels.append((line,  OPTIONS[label]))
+try:
+    with open(FILE, 'r') as file:
+        for i,line in enumerate(file.readlines()):
+            line = line.replace('\n', '').strip(',')
+            #print(line)
+            #print(f'{PROMPT}\nSelect the number for the valid option:')
+            #pprint(OPTIONS)
+            #print()
+            #label = int(input('Select an option: '))
+            #labels.append((line,  OPTIONS[label]))
+            print((line,  OPTIONS[int(selections[i])]))
+            labels.append((line,  OPTIONS[int(selections[i])]))
 
-print('All samples have been labelled')
+    print('All samples have been labelled')
 
-with open(f'labelled_{FILE}', 'w') as outfile:
+except KeyboardInterrupt:
+    if input('Continue with saving? y/N: ').lower() == 'y':
+        pass
+    else:
+        print('Quitting')
+        quit()
+
+with open(SAVEFILE, 'w+') as outfile:
     for line in labels:
-        outfile.writelines(line)
+        outfile.write(f'{line[0]}, label: {line[1]}\n')
 
 
